8403c508f533db26480d83e98d4b59963d9da6f4
alvinKyle <alvinkyle70@gmail.com>
2024-03-31 11:08:21 +0300

    updated lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+main.remove();
+
+let newHeader = document.createElement('h1');
+    console.log(newHeader);
+
+    newHeader.id = 'victory';
+    newHeader.textContent = "Alvin is the champion";
